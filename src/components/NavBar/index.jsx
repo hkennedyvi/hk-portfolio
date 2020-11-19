861d2c82e7adbf16f0f0fb8d3df77d2361e8376c
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-19 14:25:38 -0800

    navbar v1 animation

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
 
 function NavBar() {
+
+    const [navBarStatus, setNavBarStatus] = useState('navbar_closed');
+
+    function toggleNavBar(e) {
+        e.preventDefault();
+        // setNavBarStatus(!navBarStatus);
+        setNavBarStatus(navBarStatus === 'navbar_open' ? 'navbar_closed' : 'navbar_open');
+    }
+
     return (
-        <section>
-            NAVBAR
-        </section>
+        <div>
+            <button onClick={toggleNavBar} className="burger">
+                <div className="burger_bar"></div>
+                <div className="burger_bar"></div>
+                <div className="burger_bar"></div>
+            </button>
+            <nav className={`navbar ${navBarStatus}`}>
+                <ul className="nav_list">
+                    <li className="nav_item">
+                        <a className="nav_link" href="/">
+                            <span className="link_text">
+                                <button onClick={toggleNavBar}>
+                                    X
+                                </button>
+                            </span>
+                        </a>
+                    </li>
+                    <li className="nav_item">
+                        <a className="nav_link" href="/">
+                            <span className="link_text">
+                                Home
+                        </span>
+                        </a>
+                    </li>
+                    <li className="nav_item">
+                        <a className="nav_link" href="/">
+                            <span className="link_text">
+                                Home
+                        </span>
+                        </a>
+                    </li>
+                    <li className="nav_item">
+                        <a className="nav_link" href="/">
+                            <span className="link_text">
+                                Home
+                        </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
