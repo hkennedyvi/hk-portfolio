c1987134f2814d1c5c176ce0cab4b46a5a66e0ae
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-19 12:57:51 -0800

    navbar/footer create

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.scss';
+
+function NavBar() {
+    return (
+        <section>
+            NAVBAR
+        </section>
+    )
+}
+
+export default NavBar;
