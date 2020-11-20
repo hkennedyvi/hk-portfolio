86323a41251d9389fac4b5868cd367bf7de65716
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-19 16:28:16 -0800

    footer v1, navbar edit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,11 @@ import './footer.scss';
 
 function Footer() {
     return (
-        <section>
-            FOOTER
+        <section className="footer">
+            <div className="icon_container">
+                <i className="fab fa-github-square social_icon"></i>
+                <i className="fab fa-linkedin social_icon"></i>
+            </div>
         </section>
     )
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,13 +7,12 @@ function NavBar() {
 
     function toggleNavBar(e) {
         e.preventDefault();
-        // setNavBarStatus(!navBarStatus);
         setNavBarStatus(navBarStatus === 'navbar_open' ? 'navbar_closed' : 'navbar_open');
-    }
+    };
 
     return (
         <div>
-            <button onClick={toggleNavBar} className="burger">
+            <button onClick={toggleNavBar} className="burger nav_button">
                 <div className="burger_bar"></div>
                 <div className="burger_bar"></div>
                 <div className="burger_bar"></div>
@@ -23,7 +22,7 @@ function NavBar() {
                     <li className="nav_item">
                         <a className="nav_link" href="/">
                             <span className="link_text">
-                                <button onClick={toggleNavBar}>
+                                <button className="nav_button" onClick={toggleNavBar}>
                                     X
                                 </button>
                             </span>
@@ -33,21 +32,21 @@ function NavBar() {
                         <a className="nav_link" href="/">
                             <span className="link_text">
                                 Home
-                        </span>
+                            </span>
                         </a>
                     </li>
                     <li className="nav_item">
                         <a className="nav_link" href="/">
                             <span className="link_text">
-                                Home
-                        </span>
+                                About
+                            </span>
                         </a>
                     </li>
                     <li className="nav_item">
                         <a className="nav_link" href="/">
                             <span className="link_text">
-                                Home
-                        </span>
+                                Contact
+                            </span>
                         </a>
                     </li>
                 </ul>
