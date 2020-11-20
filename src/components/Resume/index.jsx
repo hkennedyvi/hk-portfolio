3be15dbd788f6c8e5c1a6ebae1f88d780e4c64c3
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-20 14:18:21 -0800

    dev icons added

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -1,50 +1,66 @@
 import React from 'react';
 import './resume.scss';
+import Amazon from './icons/amazon';
+import Bootstrap from './icons/bootstrap';
+import Django from './icons/django';
+import Handlebars from './icons/handlebars';
+import Heroku from './icons/heroku';
+import Jquery from './icons/jquery';
+import Mongo from './icons/mongo';
+import Mysql from './icons/mysql';
+import Node from './icons/node';
+import Npm from './icons/npm';
+import Postgres from './icons/postgres';
+import Python from './icons/python';
+import ReactIcon from './icons/reacticon';
+import Sass from './icons/sass';
+import Vue from './icons/vue';
+import Yarn from './icons/yarn';
+import Webpack from './icons/webpack';
 
 function Resume() {
     return (
         <div className="about_container">
             <div className="resume_container">
 
-                <div>
+                <div className="entry_left">
                     Name:
                 </div>
-                <div>
+                <div className="entry_right">
                     Howard Kennedy
                 </div>
 
-                <div>
-                    Education
+                <div className="entry_left">
+                    Education:
                 </div>
-                <div>
+                <div className="entry_right">
                     <div>PSU</div>
                     <div>U of O</div>
                 </div>
 
-                <div>
-                    Languages
+                <div className="entry_left">
+                    Languages:
                 </div>
-                <div>
-                    <div>HTML5</div>
-                    <div>U of O</div>
+                <div className="entry_right">
+                    <ReactIcon />
+                    <Django />
+                    <Sass />
+                    <Python />
+                    <Amazon />
+                    <Bootstrap />
+                    <Vue />
+                    <Npm />
+                    <Mongo />
+                    <Yarn />
+                    <Handlebars />
+                    <Postgres />
+                    <Heroku />
+                    <Jquery />
+                    <Mysql />
+                    <Node />
+                    <Webpack />
                 </div>
-                {/* <div className="entry">
-                    <div className="left_column">
-                        Name:
-                    </div>
-                    <div className="right_column">
-                        Howard Kennedy
-                    </div>
-                </div>
-                <div className="entry">
-                    <div className="left_column">
-                        Education:
-                    </div>
-                    <div className="right_column">
-                        <span>PSU</span>
-                        <span>U of O</span>
-                    </div>
-                </div> */}
+
             </div>
        RESUME
         </div>
