191966bb1d3b3ddb4fb733ba83ef40bf0bb7f7cf
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-20 17:37:14 -0800

    projects iterates, additional projects added

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,61 +1,104 @@
 import React from 'react';
 import './projects.scss';
-import restaurant from './images/restaurant.png';
-import shopper from './images/shopper.png';
+import Restaurant from './images/restaurant.png';
+import Shopper from './images/shopper.png';
+import Cars from './images/cars.png';
+import Bloggy from './images/bloggy.png';
+import AlreadyBooked from './images/alreadybooked.png';
 
 function Projects() {
+
+  const myProjects = [
+    {
+      number: 1,
+      title: "good restaurant",
+      description: "Website for a fictional restaurant.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "https://goodrestaurant.herokuapp.com/",
+      repo: "https://github.com/hkennedyvi/foodapp",
+      image: Restaurant
+    },
+    {
+      number: 2,
+      title: "shopper",
+      description: "Website for a fictional online shoe store.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "https://master.d3d58nhol1cgjn.amplifyapp.com/",
+      repo: "https://github.com/hkennedyvi/store",
+      image: Shopper
+    },
+    {
+      number: 3,
+      title: "cars",
+      description: "Website for a fictional online shoe store.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "/",
+      repo: "https://github.com/hkennedyvi/project",
+      image: Cars
+    },
+    {
+      number: 4,
+      title: "bloggy",
+      description: "Website for a fictional online shoe store.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "https://i-bloggy.herokuapp.com/",
+      repo: "https://github.com/hkennedyvi/bloggy",
+      image: Bloggy
+    },
+    {
+      number: 5,
+      title: "i'm already booked",
+      description: "Website for a fictional online shoe store.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "https://imalreadybooked.herokuapp.com/",
+      repo: "https://github.com/hkennedyvi/google-books",
+      image: AlreadyBooked
+    },
+    {
+      number: 5,
+      title: "shopper",
+      description: "Website for a fictional online shoe store.",
+      objective: "Responsive, mobile-design with from scratch animations.",
+      link: "https://master.d3d58nhol1cgjn.amplifyapp.com/",
+      repo: "https://github.com/hkennedyvi/store",
+      image: Shopper
+    },
+  ]
 
   return (
     <div className="projects_container">
       {/* <div className="test_circle">
           </div> */}
       <div className="projects_grid">
-        <div className="project">
-          <h1 className="project_header">
-            Project 1
-            </h1>
-          <h2 className="project_subheader">
-            good restaurant.
-            </h2>
-        </div>
-        <div className="project">
-          <img src={restaurant} alt="" />
-          <h3>Website for a fictional restaurant.</h3>
-          <h3>Responsive, mobile-design with from scratch animations.</h3>
-          <div>
-          <a className="projects_link" href="/">
-              LINK &nbsp;
-              <i class="fas fa-link"></i>
-            </a>
-            <a className="projects_link" href="/">
-              REPO &nbsp;
-              <i class="fas fa-code"></i>
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <h1 className="project_header">
-            Project 2
-            </h1>
-          <h2 className="project_subheader">
-            shopper.
-            </h2>
-        </div>
-        <div className="project">
-          <img src={shopper} alt="" />
-          <h3>Website for a fictional restaurant.</h3>
-          <h3>Responsive, mobile-design with from scratch animations.</h3>
-          <div>
-          <a className="projects_link" href="/">
-              LINK &nbsp;
-              <i class="fas fa-link"></i>
-            </a>
-            <a className="projects_link" href="/">
-              REPO &nbsp;
-              <i class="fas fa-code"></i>
-            </a>
-          </div>
-        </div>
+      {myProjects.map((project) => {
+          return (
+            <React.Fragment>
+              <div className="project">
+                <h1 className="project_header">
+                  Project {project.number}
+                        </h1>
+                <h2 className="project_subheader">
+                  {project.title}
+                </h2>
+              </div>
+              <div className="project">
+                <img src={project.image} alt="" />
+                <h3>{project.description}</h3>
+                <h3>{project.objective}</h3>
+                <div>
+                  <a className="projects_link" href={project.link} target="_blank">
+                    LINK &nbsp;
+                          <i class="fas fa-link"></i>
+                  </a>
+                  <a className="projects_link" href={project.repo} target="_blank">
+                    REPO &nbsp;
+                          <i class="fas fa-code"></i>
+                  </a>
+                </div>
+              </div>
+              </React.Fragment>
+          )
+        })};
       </div>
     </div>
   )
