5a8af329fcba26432fdbcacc4d1a7a439410b7bd
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-22 17:10:21 -0800

    resume grid and styling

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import './resume.scss';
 import Amazon from './icons/amazon';
 import Bootstrap from './icons/bootstrap';
+import Css from './icons/css';
 import Django from './icons/django';
-import Handlebars from './icons/handlebars';
+import Figma from './icons/figma.png';
 import Heroku from './icons/heroku';
+import Html from './icons/html';
+import Javascript from './icons/javascript';
 import Jquery from './icons/jquery';
 import Mongo from './icons/mongo';
 import Mysql from './icons/mysql';
 import Node from './icons/node';
-import Npm from './icons/npm';
+import Photoshop from './icons/photoshop';
 import Postgres from './icons/postgres';
 import Python from './icons/python';
 import ReactIcon from './icons/reacticon';
 import Sass from './icons/sass';
 import Vue from './icons/vue';
-import Yarn from './icons/yarn';
 import Webpack from './icons/webpack';
 
 function Resume() {
@@ -39,26 +41,100 @@ function Resume() {
                 </div>
 
                 <div className="entry_left">
+                    Design Tools:
+                </div>
+                <div className="entry_right icon_grid">
+                    <div className="entry"><img id="figma_icon" src={Figma} alt="" />Figma</div>
+                    <div className="entry"><Photoshop />Photoshop Elements</div>
+                </div>
+                <div className="entry_left">
                     Languages:
                 </div>
-                <div className="entry_right">
-                    <ReactIcon />
-                    <Django />
-                    <Sass />
-                    <Python />
-                    <Amazon />
-                    <Bootstrap />
-                    <Vue />
-                    <Npm />
-                    <Mongo />
-                    <Yarn />
-                    <Handlebars />
-                    <Postgres />
-                    <Heroku />
-                    <Jquery />
-                    <Mysql />
-                    <Node />
-                    <Webpack />
+                <div className="entry_right icon_grid">
+                    <div className="entry">
+                        <Html />
+                        <span className="entry_text">HTML</span>
+                    </div>
+                    <div className="entry">
+                        <Javascript />
+                        <span>Javascipt</span>
+                    </div>
+                    <div className="entry">
+                        <Node />
+                        <span>Node JS</span>
+                    </div>
+                    <div className="entry">
+                        <Jquery />
+                        <span>JQuery</span>
+                    </div>
+                    <div className="entry">
+                        <Python />
+                        <span>Python</span>
+                    </div>
+                </div>
+                <div className="entry_left">
+                    Frameworks:
+                </div>
+                <div className="entry_right icon_grid">
+                    <div className="entry">
+                        <ReactIcon />
+                        <span>React</span>
+                    </div>
+                    <div className="entry">
+                        <Vue />
+                        <span>VueJS</span>
+                    </div>
+                    <div className="entry">
+                        <Django />
+                        <span>Django</span>
+                    </div>
+                </div>
+                <div className="entry_left">
+                    Styling:
+                </div>
+                <div className="entry_right icon_grid">
+                    <div className="entry">
+                        <Css />
+                        <span>CSS</span>
+                    </div>
+                    <div className="entry">
+                        <Sass />
+                        <span>Sass</span>
+                    </div>
+                    <div className="entry">
+                        <Bootstrap />
+                        <span>Bootstrap</span>
+                    </div>
+                </div>
+                <div className="entry_left">
+                    Hosting:
+                </div>
+                <div className="entry_right icon_grid">
+                    <div className="entry">
+                        <Amazon />
+                        <span>Amazon Web Services</span>
+                    </div>
+                    <div className="entry">
+                        <Heroku />
+                        <span>Heroku</span>
+                    </div>
+                </div>
+                <div className="entry_left">
+                    Databases:
+                </div>
+                <div className="entry_right icon_grid">
+                    <div className="entry">
+                        <Mongo />
+                        <span>MongoDB</span>
+                    </div>
+                    <div className="entry">
+                        <Postgres />
+                        <span>PostgreSQL</span>
+                    </div>
+                    <div className="entry">
+                        <Mysql />
+                        <span>MySQL</span>
+                        </div>
                 </div>
             </div>
         </div>
